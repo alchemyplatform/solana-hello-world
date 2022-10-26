4c4ca5b96806ff3bbd163a0c766941024b2a887d
avasisht23 <ajay@alchemy.com>
2022-10-25 22:54:14 -0400

    Step 6: setting up the testing environment

--- tests/solana-hello-world.ts
+++ tests/solana-hello-world.ts
@@ -1,16 +1,12 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaHelloWorld } from "../target/types/solana_hello_world";
+import * as assert from "assert";
 
 describe("solana-hello-world", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.SolanaHelloWorld as Program<SolanaHelloWorld>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  const program = anchor.workspace
+    .SolanaHelloWorld as Program<SolanaHelloWorld>;
 });
