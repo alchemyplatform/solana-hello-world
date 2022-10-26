34d403b736f2da2e7dcf6bf748ade10a9c866897
avasisht23 <ajay@alchemy.com>
2022-10-25 22:20:00 -0400

    Step 5: build and deploy your solana program to the devnet

--- programs/solana-hello-world/src/lib.rs
+++ programs/solana-hello-world/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Ecr5UgGyJTnKraLRPYBXrK9RD7DfoVDYfSioVksnnfjA");
 
 #[program]
 pub mod solana_hello_world {
