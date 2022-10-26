44cc2eabb43bfad44be51d003a0deea2d95fb043
avasisht23 <ajay@alchemy.com>
2022-10-25 21:18:42 -0400

    Step 3: defining the instruction context

--- programs/solana-hello-world/src/lib.rs
+++ programs/solana-hello-world/src/lib.rs
@@ -12,7 +12,22 @@ pub mod solana_hello_world {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateMessage<'info> {
+    #[account(init, payer = author, space = 1000)]
+    pub message: Account<'info, Message>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateMessage<'info> {
+    #[account(mut)]
+    pub message: Account<'info, Message>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct Message {
