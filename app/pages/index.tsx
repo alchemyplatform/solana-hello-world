6393279cd5c95697465c7d641da7950e8f160ef4
avasisht23 <ajay@alchemy.com>
2022-11-02 13:38:29 -0400

    Step 13: Making a Create Message API

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import { Keypair } from "@solana/web3.js";
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import useIsMounted from "./api/utils/useIsMounted";
+import createMessage from "./api/createMessage";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [messageAccount, _] = useState(Keypair.generate());
+  const [message, setMessage] = useState("");
+  const [messageAuthor, setMessageAuthor] = useState("");
+  const [messageTime, setMessageTime] = useState(0);
+  const [inputtedMessage, setInputtedMessage] = useState("");
+
+  const wallet = useAnchorWallet();
   const mounted = useIsMounted();
 
   return (
@@ -14,6 +25,48 @@ export default function Home() {
           Your First Solana Program with{" "}
           <a href="https://alchemy.com/solana/?a=d0c917f7ef">Alchemy</a>!
         </h1>
+
+        {wallet && (
+          <div className={styles.message_bar}>
+            <input
+              className={styles.message_input}
+              placeholder="Write Your Message!"
+              onChange={(e) => setInputtedMessage(e.target.value)}
+              value={inputtedMessage}
+            />
+            <button
+              className={styles.message_button}
+              disabled={!inputtedMessage}
+              onClick={async () => {
+                const message = await createMessage(
+                  inputtedMessage,
+                  wallet,
+                  messageAccount
+                );
+                if (message) {
+                  setMessage(message.content.toString());
+                  setMessageAuthor(message.author.toString());
+                  setMessageTime(message.timestamp.toNumber() * 1000);
+                  setInputtedMessage("");
+                }
+              }}
+            >
+              Create a Message!
+            </button>
+          </div>
+        )}
+
+        {wallet && message && (
+          <div className={styles.card}>
+            <h2>Current Message: {message}</h2>
+            <h2>
+              Message Author: {messageAuthor.substring(0, 4)}
+              ...
+              {messageAuthor.slice(-4)}
+            </h2>
+            <h2>Time Published: {new Date(messageTime).toLocaleString()}</h2>
+          </div>
+        )}
       </div>
     </div>
   );
