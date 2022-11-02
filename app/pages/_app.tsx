207f77372e6f88a2b882af571f8b53455b6608b1
avasisht23 <ajay@alchemy.com>
2022-11-02 12:45:17 -0400

    Step 12: Integrating Your Phantom Wallet

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { endpoint } from "./api/utils/constants";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const phantomWallet = new PhantomWalletAdapter();
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[phantomWallet]}>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
+
+export default MyApp;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,8 +1,14 @@
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import useIsMounted from "./api/utils/useIsMounted";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const mounted = useIsMounted();
+
   return (
     <div className={styles.container}>
+      <div className={styles.navbar}>{mounted && <WalletMultiButton />}</div>
+
       <div className={styles.main}>
         <h1 className={styles.title}>
           Your First Solana Program with{" "}
